59a01f226793f586d6cde56bcec387e1d7ea7d28
michaelovo <emikeovo@gmail.com>
2019-05-17 07:35:44 -0700

    Added Application server file configuration

---
+++ server.js
@@ -0,0 +1,43 @@
+/* IMPORT ALL NODE MODULES
+*/
+
+//CORE NODE MODULES
+var express = require('express');
+var http = require('http');
+var mysql = require('mysql');
+var app = express();
+var bodyParser = require('body-parser');
+
+//PARSE ALL FORM DATA
+app.use(bodyParser.urlencoded({extended: true}));
+
+//USED FOR FORMATTING DATE
+var dateFormat = require('dateformat');
+var now = new Date();
+
+//TO PARSE VIEW ENGINE TEMPLATE(EJS)
+app.set('view engine', 'ejs');
+//app.set('Content-Type', 'text/css');
+
+//IMPORT ALL JS AND CSS FILES FOR THE PROJECT
+app.use(express.static('resources'))
+
+//MYSQL DATABASE CONNECTION
+/*
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "node_crud"
+});
+*/
+//GLOBAL SITE TITLE AND BASE URL
+const siteTitle = "Nodejs CRUD app";
+const baseURL ="http://localhost:3000/";
+
+
+
+//CONNECTION TO THE SERVER
+var server = app.listen(3000,function(){
+  console.log("server started on 3000");
+});
